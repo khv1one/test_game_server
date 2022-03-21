3da5ebfb68fa24e38bef8a81593cda4d36485c47
Хвостовец Андрей <khvostovets@kinoplan.ru>
2022-03-22 00:14:23 +0300

    add universal card logic

--- gameserver/src/main/scala/org/khvostovets/gameserver/game/card/CardGame.scala
+++ gameserver/src/main/scala/org/khvostovets/gameserver/game/card/CardGame.scala
@@ -1,8 +1,8 @@
 package org.khvostovets.gameserver.game.card
 
-import cats.data.{NonEmptyList, OptionT}
+import cats.data.NonEmptyList
 import cats.effect.kernel.Async
-import cats.implicits.{catsSyntaxApplicativeId, catsSyntaxOptionId, toFlatMapOps, toFoldableOps, toFunctorOps, toTraverseOps}
+import cats.implicits.{catsSyntaxApplicativeId, toFlatMapOps, toFoldableOps, toFunctorOps, toTraverseOps}
 import org.khvostovets.gameserver.game._
 import org.khvostovets.gameserver.message.{OutputMessage, SendToUser}
 
@@ -12,24 +12,22 @@ case class CardGame[F[_] : Async](
   users: NonEmptyList[String],
   usersCards: Map[String, Deck[F]],
   userDecisions: Map[String, GameAction],
-  isFinish: Boolean = false
+  cardHandSize: Int = 1
 ) {
 
   def next: GameAction => F[(CardGame[F], Seq[GameResult], Seq[OutputMessage])] = {
-    case action : Play =>
-      val (game, playMessages) = play(action)
-
-      game.isEnd().map { case (results, messages) => (game, results, messages ++ playMessages)}
-
-    case action : Fold =>
-      val (game, playMessages) = play(action)
-
-      game.isEnd().map { case (results, messages) => (game, results, messages ++ playMessages)}
-
+    case action : Play => process(action)
+    case action : Fold => process(action)
     case action =>
       val messages: Seq[OutputMessage] = Seq(SendToUser(action.user, "Unknown command"))
 
       (this, Seq.empty[GameResult], messages).pure[F]
+  }
+
+  private def process(action: GameAction): F[(CardGame[F], Seq[GameResult], Seq[OutputMessage])] = {
+    val (game, playMessages) = play(action)
+
+    game.isEnd.map { case (game, results, messages) => (game, results, messages ++ playMessages)}
   }
 
   private def play(action: GameAction): (CardGame[F], Seq[SendToUser]) = {
@@ -37,7 +35,8 @@ case class CardGame[F[_] : Async](
       .get(action.user)
       .fold {
         (
-          copy(userDecisions = userDecisions + (action.user -> action)),
+          new CardGame(users, usersCards, userDecisions + (action.user -> action), cardHandSize), //TODO
+          //copy(userDecisions = userDecisions + (action.user -> action)),
           msgToUserAndOther(action.user, "You chose to play", s"Player ${action.user} has made a decision")
         )
       } { _ =>
@@ -45,19 +44,19 @@ case class CardGame[F[_] : Async](
       }
   }
 
-  private def isEnd(): F[(Seq[GameResult], Seq[OutputMessage])] = {
+  private def isEnd: F[(CardGame[F], Seq[GameResult], Seq[OutputMessage])] = {
     if (userDecisions.keys.size >= users.size) {
-      winnerCalculation().map { results =>
+      winnerCalculation().map { case (game, results) =>
         val messages = results.map(result => SendToUser(result.user, result.toString))
 
-        (results, messages)
+        (game, results, messages)
       }
     } else {
-      (Seq.empty[GameResult], Seq.empty[OutputMessage]).pure[F]
+      (this, Seq.empty[GameResult], Seq.empty[OutputMessage]).pure[F]
     }
   }
 
-  private def winnerCalculation(): F[Seq[GameResult]] = {
+  private def winnerCalculation(): F[(CardGame[F], Seq[GameResult])] = {
     val (players, folders) =
       userDecisions
         .foldLeft(Seq.empty[String], Seq.empty[String]) { case ((players, folders), (user, action)) => action match {
@@ -66,45 +65,48 @@ case class CardGame[F[_] : Async](
         }}
 
     if (players.isEmpty) {
-      folders.map(user => GameResult(user, -2)).pure[F]
+      (this, folders.map(user => GameResult(user, -2))).pure[F]
     } else if (players.size == 1) {
-      (folders.map(user => GameResult(user, -5)) :+ GameResult(players.head, 5)).pure[F]
+      (this, folders.map(user => GameResult(user, -5)) :+ GameResult(players.head, 5)).pure[F]
     } else {
-      compareDeck(players).map { case (winnerO, losers) =>
+      compareUsersMaxCards(players).flatMap { case (winnerO, losers) =>
 
         winnerO.fold(
-          Seq.empty[GameResult]
+          CardGame[F](NonEmptyList.fromList(losers.toList).get)
+            .map { case (game, _) => (game, Seq.empty[GameResult]) } //TODO need to customise message when game restart
         ) { winner =>
-          folders.map(user => GameResult(user, -5)) ++ losers.map(user => GameResult(user, -20)) :+ GameResult(winner, 20)
+          (this, folders.map(user => GameResult(user, -5)) ++ losers.map(user => GameResult(user, -20)) :+ GameResult(winner, 20)).pure[F]
         }
       }
     }
   }
 
-  private def compareDeck(users: Seq[String]): F[(Option[String], Seq[String])] = {
-    val usersSet = users.toSet
-
-    usersCards
-      .filter { case (key, _) => usersSet.contains(key)}
-      .toList
-      .foldLeftM(Option.empty[String], Option.empty[Deck[F]]) { case ((userAccO, deckAccO), (user, deck)) =>
-        deckAccO.fold { (user.some, deck.some).pure[F] } { deckAcc =>
-          (for {
-            accCard <- OptionT(deckAcc.getMaxCard())
-            card <- OptionT(deck.getMaxCard())
-          } yield {
-            if (card.compareByRank(accCard) > 0) {
-              (user.some, deck.some)
-            } else {
-              (userAccO, deckAccO)
-            }
-          })
-            .fold((Option.empty[String], Option.empty[Deck[F]])) { a => a}
+  private def compareUsersMaxCards(users: Seq[String], countAcc: Int = 0): F[(Option[String], Seq[String])] = {
+    if (countAcc < cardHandSize) {
+      usersCards
+        .filter { case (key, _) => users.contains(key)}
+        .toList
+        .traverse { case (user, deck) =>
+          deck.getMaxCard()
+            .map(cardO => (user, cardO.map(_.rank.score).getOrElse(0))) //TODO really? getOrElse(0)?
         }
-      }
-      .map { case (winnerO, _) =>
-        (winnerO, winnerO.fold(users)(winner => (usersSet - winner).toSeq))
-      }
+        .flatMap { scoreByUser =>
+          val (_, usersWithMaxScores) = scoreByUser.groupBy { case (_, scores) => scores }
+            .map { case (score, usersWithScore) => (score, usersWithScore.map { case (user, _) => user }) }
+            .toList
+            .maxBy { case (score, _) => score }
+
+          if (usersWithMaxScores.size > 1) {
+            compareUsersMaxCards(usersWithMaxScores, countAcc + 1)
+          } else if (usersWithMaxScores.size == 1) {
+            (usersWithMaxScores.headOption, users.filterNot(_ == usersWithMaxScores.head)).pure[F]
+          } else {
+            (Option.empty[String], Seq.empty[String]).pure[F] //TODO something went wrong
+          }
+        }
+    } else {
+      (Option.empty[String], users).pure[F]
+    }
   }
 
   private def msgToUserAndOther(user: String, messageToUser: String, messageToAnotherUsers: String): Seq[SendToUser] = {
